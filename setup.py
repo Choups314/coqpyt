9d00aa42006f3733762aaf4a4348a456c4928ecb
Choups314 <barnabe.chauvin@gmail.com>
2015-04-13 20:50:08 +0200

    Add a setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='coqpyth', \
+    version='1.0',
+    description='Coq API',
+    author='Barnabe Chauvin',
+    author_email='barnabe.chauvin@gmail.com',
+    url='https://github.com/Choups314/coqpyt',
+    packages=['coqpyth'],
+)
